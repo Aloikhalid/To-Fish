85a2d0006665ac82b01c2a0acc6b32335e8b0f42
Alya Alabdulrahim <AliaKhalidAlabdulrahim@gmail.com>
2026-03-10 01:18:35 +0300

    commented out some files

--- ToFishWidget/ToFishWidgetBundle.swift
+++ ToFishWidget/ToFishWidgetBundle.swift
@@ -4,15 +4,15 @@
 //
 //  Created by alya Alabdulrahim on 21/09/1447 AH.
 //
-
-import WidgetKit
-import SwiftUI
-
-@main
-struct ToFishWidgetBundle: WidgetBundle {
-    var body: some Widget {
-        AquariumWidget()
-        ToFishWidgetControl()
-        ToFishWidgetLiveActivity()
-    }
-}
+//
+//import WidgetKit
+//import SwiftUI
+//
+//@main
+//struct ToFishWidgetBundle: WidgetBundle {
+//    var body: some Widget {
+//        AquariumWidget()
+//        ToFishWidgetControl()
+//        ToFishWidgetLiveActivity()
+//    }
+//}
